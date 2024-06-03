087154536424120462b0bf15475c2056dc626416
Jacob <wanni@vip.qq.com>
2024-06-03 18:02:21 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,174 +1,221 @@
 addEventListener('fetch', event => {
-  // When a fetch event occurs, responds with the result of the handleRequest function
   event.respondWith(handleRequest(event.request));
 });
 
-async function parseResponseByContentType(response, contentType) {
-  // If there's no content type, the response is returned as text
-  if (!contentType) return await response.text();
-
-  // Depending on the content type, different actions are taken
-  switch (true) {
-    case contentType.includes('application/json'):
-      // If the content type is JSON, the response is returned as a JSON string
-      return JSON.stringify(await response.json());
-    case contentType.includes('text/html'):
-      // If the content type is HTML, the response is transformed using HTMLRewriter
-      const transformedResponse = new HTMLRewriter()
-        .on('body', {
-          element(element) {
-            // Custom CSS and JS can be added into the body of the HTML
-            element.append(
-              `
-                <style>
-                  // Custom CSS you can add to
-                  // modify the styling of your page
-                </style>
-                `,
-              { html: true },
-            );
-            element.append(
-              `
-                <script>
-                  // Custom JS you can add to
-                  // modify something on your page
-                </script>
-                `,
-              { html: true },
-            );
-          },
-        })
-        .transform(response);
-      // The transformed response is returned as text
-      return await transformedResponse.text();
-
-    case contentType.includes('font'):
-      // If the content type is a font, the response is returned as an ArrayBuffer
-      return await response.arrayBuffer();
-
-    case contentType.includes('image'):
-      // If the content type is an image, the response is returned as an ArrayBuffer
-      return await response.arrayBuffer()
-
-    default:
-      // If the content type is anything else, the response is returned as text
-      return await response.text();
-  }
-}
+addEventListener('scheduled', event => {
+  event.waitUntil(handleScheduled(event));
+});
 
 async function handleRequest(request) {
   const url = new URL(request.url);
   const path = url.pathname;
 
-  // 基本的请求头部定义
+  // 目录保护逻辑
+  if (!isValidPath(path, request.method)) {
+    return new Response('Forbidden', { status: 403 });
+  }
+
+  if (path === '/cleanup' && request.method === 'POST') {
+    return await cleanupR2Bucket();
+  }
+
+  const referer = request.headers.get("referer") || '';
   let headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
   };
 
-  // 检查是否是特定文件请求
-  if (isDirectFileRequest(path)) {
-    try {
-      const object = await R2_BUCKET.get(path.substring(1)); // 去掉路径中的首个斜杠
-      if (!object) {
-        console.log("No object found in R2.");
-        return new Response('File not found', { status: 404 });
+  if (isStaticResourceRequest(path)) {
+    const fileResponse = await fetchFromR2(path);
+    if (fileResponse) {
+      console.log(`File ${path} served from R2`);
+      return applyReplacementsToResponse(fileResponse, headers);
+    } else {
+      const originUrl = determineOriginUrl(path, referer);
+      if (!originUrl) {
+        return new Response('Unauthorized access', { status: 403 });
       }
-
-      console.log("Object found:", object);
-      console.log("HTTP Metadata:", object.httpMetadata);
-
-      const arrayBuffer = await object.arrayBuffer();
-      const contentType = object.httpMetadata?.contentType || determineContentType(path);
-      return new Response(arrayBuffer, {
-        headers: { 'Content-Type': contentType }
-      });
-    } catch (error) {
-      console.error("Error fetching file:", error);
-      return new Response(`Error fetching file: ${error.message}`, { status: 500 });
+      const fetchedResponse = await fetchAndStoreFile(path, headers, originUrl);
+      console.log(`File ${path} served from origin via reverse proxy`);
+      return applyReplacementsToResponse(fetchedResponse, headers);
     }
   }
 
-  // 特殊处理 /v1/users/me
-  if (path === '/v1/users/me') {
-    const jsonBody = JSON.stringify({
-      "status": 401,
-      "code": "UNKNOWN_ERROR",
-      "message": "Unauthorized"
-    });
-    return new Response(jsonBody, {
-      headers: { 'content-type': 'application/json' },
-      status: 401
-    });
+  let targetUrl = determineOriginUrl(path, referer);
+  if (!targetUrl) {
+    return new Response('Unauthorized access', { status: 403 });
   }
 
-  // 根目录重定向到个人页面
-  if (path === '/') {
-    const targetUrl = 'https://bento.me/' + BENTO_USERNAME;
-    const response = await fetch(targetUrl, { headers });
-    const contentType = response.headers.get('content-type');
-    let results = await parseResponseByContentType(response, contentType);
-
-    // 替换策略
-    if (!(results instanceof ArrayBuffer)) {
-      results = results.replaceAll('https://api.bento.me', BASE_URL); // 修正CORS问题
-      results = results.replaceAll('pk.eyJ1IjoibXVnZWViIiwiYSI6ImNsdG5idzFrbTA0c3UycnA4OWRtbTJ6dmMifQ.Qa0vYWIbFEHuNuPpbVkdEQ', MAPBOX_TOKEN); // 替换mapbox的token，解决域名限制
-      results = results.replaceAll('flex w-full flex-col items-center bg-[#FBFBFB]', 'hidden'); // 屏蔽底部登录元素
-      results = results.replaceAll('fixed left-16 bottom-[52px] -m-1 hidden items-center space-x-1 rounded-[12px] p-1 transition-colors xl:flex 2xl:space-x-2', 'hidden'); // 屏蔽底部登录元素
-    }
-
-    headers['content-type'] = contentType;
-    return new Response(results, { headers });
-  }
-
-  // 允许的资源路径检查
-  const allowedPatterns = [
-    '/v1', '/_next', '/images', '/_axiom'
-  ];
-  const isAllowed = allowedPatterns.some(pattern => path.startsWith(pattern));
-
-  if (!isAllowed) {
-    return new Response('Not Allowed', { status: 403 });
-  }
-
-  // 允许的资源路径，直接代理到 bento.me
-  const targetUrl = 'https://bento.me' + path;
   const response = await fetch(targetUrl, { headers });
   const contentType = response.headers.get('content-type');
   let results = await parseResponseByContentType(response, contentType);
 
-  // 替换策略
-  if (!(results instanceof ArrayBuffer)) {
-    results = results.replaceAll('https://api.bento.me', BASE_URL); // 修正CORS问题
-    results = results.replaceAll('pk.eyJ1IjoibXVnZWViIiwiYSI6ImNsdG5idzFrbTA0c3UycnA4OWRtbTJ6dmMifQ.Qa0vYWIbFEHuNuPpbVkdEQ', MAPBOX_TOKEN); // 替换mapbox的token，解决域名限制
-    results = results.replaceAll('flex w-full flex-col items-center bg-[#FBFBFB]', 'hidden'); // 屏蔽底部登录元素
-    results = results.replaceAll('fixed left-16 bottom-[52px] -m-1 hidden items-center space-x-1 rounded-[12px] p-1 transition-colors xl:flex 2xl:space-x-2', 'hidden'); // 屏蔽底部登录元素
+  if (contentType.includes('text/') || contentType.includes('application/json') || contentType.includes('application/javascript')) {
+    results = applyReplacements(results);
   }
 
   headers['content-type'] = contentType;
   return new Response(results, { headers });
 }
 
-function isDirectFileRequest(path) {
-  // 定义允许的直接文件请求路径
-  const directFileRules = [
-    '/favicon.ico',
-    '/site.webmanifest',
-    /^\/[^\/]+\.png$/  // 匹配根目录下的任意.png文件
-  ];
+async function handleScheduled(event) {
+  return await cleanupR2Bucket();
+}
 
-  return directFileRules.some(rule => {
-    if (typeof rule === 'string') {
-      return path === rule;
-    } else if (rule instanceof RegExp) {
-      return rule.test(path);
+async function parseResponseByContentType(response, contentType) {
+  if (!contentType) return await response.text();
+
+  switch (true) {
+    case contentType.includes('application/json'):
+      let jsonResponse = await response.json();
+      return JSON.stringify(jsonResponse);
+    case contentType.includes('text/html'):
+      let htmlResponse = await response.text();
+      return new HTMLRewriter()
+        .on('body', {
+          element(element) {
+            element.append(`
+                <style>// Custom CSS</style>`, { html: true });
+            element.append(`
+                <script>// Custom JS</script>`, { html: true });
+          },
+        })
+        .transform(new Response(htmlResponse, { headers: { 'content-type': 'text/html' } }))
+        .text();
+    case contentType.includes('javascript') || contentType.includes('application/javascript') || contentType.includes('text/javascript'):
+      return await response.text();
+    case contentType.includes('text/css'):
+      return await response.text();
+    case contentType.includes('font') || contentType.includes('image'):
+      return response.arrayBuffer();
+    default:
+      return await response.text();
+  }
+}
+
+function isStaticResourceRequest(path) {
+  return path.match(/\.(jpg|jpeg|png|gif|webp|ico|webmanifest|js|css)$/i);
+}
+
+function isValidPath(path, method) {
+  const allowedGetPaths = ['/_next', '/_axiom', '/googleapis_storage', '/api', '/images'];
+  const allowedPostPaths = ['/cleanup', '/api'];
+
+  if (method === 'GET') {
+    // Allow exact root path for static files only
+    if (path === '/' || (path.startsWith('/') && isStaticResourceRequest(path) && !path.includes('/', 1))) {
+      return true;
     }
-  });
+    // Allow specific paths and their sub-paths
+    return allowedGetPaths.some(allowedPath => path === allowedPath || (path.startsWith(allowedPath) && path[allowedPath.length] === '/'));
+  }
+
+  if (method === 'POST') {
+    return allowedPostPaths.includes(path);
+  }
+
+  return false;
+}
+
+function determineOriginUrl(path, referer) {
+  if ((path.startsWith('/api') || path.startsWith('/googleapis_storage')) && !referer.startsWith(BASE_URL)) {
+    return null;
+  }
+  if (path.startsWith('/api')) {
+    return `https://api.bento.me${path.substring(4)}`;
+  } else if (path.startsWith('/googleapis_storage')) {
+    return `https://storage.googleapis.com${path.substring(19)}`;
+  } else if (path === '/') {
+    return `https://bento.me/${BENTO_USERNAME}`;
+  } else {
+    return `https://bento.me${path}`;
+  }
+}
+
+async function fetchFromR2(path) {
+  try {
+    const object = await R2_BUCKET.get(path.substring(1));
+    if (!object) {
+      return null;
+    }
+    const arrayBuffer = await object.arrayBuffer();
+    const contentType = object.httpMetadata?.contentType || determineContentType(path);
+    return new Response(arrayBuffer, { headers: { 'Content-Type': contentType } });
+  } catch (error) {
+    console.error("Error fetching from R2:", error);
+    return null;
+  }
+}
+
+async function fetchAndStoreFile(path, headers, originUrl) {
+  const response = await fetch(originUrl, { headers });
+  if (response.ok) {
+    const arrayBuffer = await response.arrayBuffer();
+    try {
+      await R2_BUCKET.put(path.substring(1), arrayBuffer, {
+        httpMetadata: {
+          contentType: response.headers.get('content-type')
+        }
+      });
+    } catch (error) {
+      console.error("Error storing file to R2:", error);
+    }
+    return new Response(arrayBuffer, {
+      headers: { 'Content-Type': response.headers.get('content-type') }
+    });
+  } else {
+    return response; // Directly return the response if there is an error
+  }
+}
+
+function applyReplacements(text) {
+  text = text.replaceAll('https://api.bento.me', `${BASE_URL}/api`);
+  text = text.replaceAll('https://storage.googleapis.com', `${BASE_URL}/googleapis_storage`);
+  text = text.replaceAll('pk.eyJ1IjoibXVnZWViIiwiYSI6ImNsdG5idzFrbTA0c3UycnA4OWRtbTJ6dmMifQ.Qa0vYWIbFEHuNuPpbVkdEQ', MAPBOX_TOKEN);
+  text = text.replaceAll('flex w-full flex-col items-center bg-[#FBFBFB]', 'hidden');
+  text = text.replaceAll('fixed left-16 bottom-[52px] -m-1 hidden items-center space-x-1 rounded-[12px] p-1 transition-colors xl:flex 2xl:space-x-2', 'hidden');
+  return text;
+}
+
+async function applyReplacementsToResponse(response, headers) {
+  const contentType = response.headers.get('Content-Type');
+  if (contentType.includes('text/') || contentType.includes('application/json') || contentType.includes('application/javascript')) {
+    const textResponse = await response.text();
+    const replacedText = applyReplacements(textResponse);
+    return new Response(replacedText, { headers: { ...headers, 'Content-Type': contentType } });
+  }
+  return response;
 }
 
 function determineContentType(path) {
   if (path.endsWith('.ico')) return 'image/x-icon';
   if (path.endsWith('.webmanifest')) return 'application/manifest+json';
-  if (path.endsWith('.png')) return 'image/png';
+  if (path.endsWith('.png') || path.endsWith('.jpg') || path.endsWith('.jpeg') || path.endsWith('.gif') || path.endsWith('.webp')) return 'image/png'; // Example, should be specific
   return 'application/octet-stream';
+}
+
+async function cleanupR2Bucket() {
+  const retentionPeriod = 3 * 24 * 60 * 60 * 1000; // 3 days in milliseconds
+  const currentTime = Date.now();
+
+  let cursor;
+  do {
+    const listResponse = await R2_BUCKET.list({ cursor });
+    const keysToDelete = [];
+
+    for (const object of listResponse.objects) {
+      const lastModified = new Date(object.uploaded).getTime();
+      if (object.key !== '' && (currentTime - lastModified) > retentionPeriod) {
+        keysToDelete.push(object.key);
+      }
+    }
+
+    if (keysToDelete.length > 0) {
+      await R2_BUCKET.delete(keysToDelete);
+    }
+
+    cursor = listResponse.cursor;
+  } while (cursor);
+
+  return new Response('Cleanup completed', { status: 200 });
 }
